81767f95f114ff80ee50db2a37469609d037998b
xpbach2508 <21020283@vnu.edu.vn>
2023-07-05 21:09:46 +0700

    Token added

--- Token.sol
+++ Token.sol
@@ -12,8 +12,24 @@ contract VotingToken {
         name = _name;
         symbol = _symbol;
         balanceOf[msg.sender] = _initialSupply;
+        totalSupply += _initialSupply;
     }
     function deposit() payable public {
         depositOf[msg.sender] = msg.value;
+        // 0.1 eth 1000token
+        uint256 totalTokenReceive = msg.value * 1000 / (0.1 * 10**18);
+        balanceOf[msg.sender] = totalTokenReceive;
+        totalSupply += totalTokenReceive;
+    }
+
+    function transfer(address _to, uint256 _value) public returns(bool success) {
+        _transfer(msg.sender, _to, _value);
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(balanceOf[_from] > _value, "Insufficient balance");
+        require(_to != address(0), "Address 0 repipient");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
     }
 }
